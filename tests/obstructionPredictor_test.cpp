0d3e4afd3eb5cc17bc44a4927bac619f52107e50
Juliusz Straszyński <julek@straszynski.pl>
2021-09-08 00:24:06 +0200

    reformatting + small changes test

--- tests/obstructionPredictor_test.cpp
+++ tests/obstructionPredictor_test.cpp
@@ -5,7 +5,7 @@
 TEST(ObstructionPredictorTest, AllZeros3h) {
     ObstructionPredictor p;
 
-    for(int i = 0; i < 100; i++){
+    for (int i = 0; i < 100; i++) {
         p.Insert(i * 3 * 60 * 60 * 1000, 0, 0);
         EXPECT_FLOAT_EQ(p.ObstructionValue(), 0);
     }
@@ -14,13 +14,13 @@ TEST(ObstructionPredictorTest, AllZeros3h) {
 TEST(ObstructionPredictorTest, OneSuperSpike) {
     ObstructionPredictor p;
 
-    for(int i = 0; i < 100; i++){
+    for (int i = 0; i < 100; i++) {
         uint64_t t = i * 3 * 60 * 60 * 1000;
-        if(i != 60){
+        if (i != 60) {
             p.Insert(t, 0, 0);
             EXPECT_FALSE(p.IsObstructed());
             EXPECT_FLOAT_EQ(p.ObstructionValue(), 0);
-        } else{
+        } else {
             p.Insert(t, 10000, 0);
             EXPECT_TRUE(p.IsObstructed());
             EXPECT_FLOAT_EQ(p.ObstructionValue(), -10000);
@@ -31,16 +31,35 @@ TEST(ObstructionPredictorTest, OneSuperSpike) {
 TEST(ObstructionPredictorTest, OneSmallSpike) {
     ObstructionPredictor p;
 
-    for(int i = 0; i < 100; i++){
+    for (int i = 0; i < 100; i++) {
         uint64_t t = i * 3 * 60 * 60 * 1000;
-        if(i != 60){
+        if (i != 60) {
             p.Insert(t, 0, 0);
             EXPECT_FALSE(p.IsObstructed());
             EXPECT_FLOAT_EQ(p.ObstructionValue(), 0);
-        } else{
+        } else {
             p.Insert(t, 100, 0);
             EXPECT_TRUE(p.IsObstructed());
             EXPECT_FLOAT_EQ(p.ObstructionValue(), -100);
         }
+    }
+}
+
+TEST(ObstructionPredictorTest, SmallChanges) {
+    ObstructionPredictor p;
+    uint64_t t_step = 3 * 60 * 60 * 1000;
+    uint64_t t = 0;
+    int h = 200;
+    for (int i = 0; i < 25; i++) {
+        p.Insert(t, h--, 200);
+        EXPECT_FALSE(p.IsObstructed());
+        EXPECT_FLOAT_EQ(p.ObstructionValue(), 0);
+        t += t_step;
+    }
+    for (int i = 0; i < 25; i++) {
+        p.Insert(t, h++, 200);
+        EXPECT_FALSE(p.IsObstructed());
+        EXPECT_FLOAT_EQ(p.ObstructionValue(), 0);
+        t += t_step;
     }
 }
