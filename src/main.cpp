4ab2d7de55e18584ad060bb1cb4bef43b655816c
Juliusz Straszyński <julek@straszynski.pl>
2021-09-13 05:07:25 +0200

    library correction

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,20 @@
 
 using std::cin;
 using std::cout;
+using std::cerr;
 using std::ifstream;
 
 #include "obstructionPredictor/ad_utils.h"
+
 int main() {
     FullAnomalyDetector fd_ad;
     time_t timestamp;
     cout << std::fixed << std::setprecision(1);
 
     double measurement;
-    while(cin >> timestamp >> measurement){
+    while (cin >> timestamp >> measurement) {
         auto r = fd_ad.add(timestamp, measurement);
-        cout << r.is_anomaly << " " << r.predicted_measurement << "\n";
+        cout << timestamp << " " << r.is_anomaly << " " << r.predicted_measurement <<" " << measurement << "\n";
     }
     return 0;
 }
--- src/obstructionPredictor/ad_utils.cpp
+++ src/obstructionPredictor/ad_utils.cpp
@@ -10,15 +10,6 @@ double median(vector<double> v) {
     return (v[size / 2] + v[(size - 1) / 2]) / 2;
 }
 
-double dot_product(const vector<double> &v1, const vector<double> &v2) {
-    assert(v1.size() == v2.size());
-    double res = 0;
-    for (int i = 0; i < static_cast<int>(v1.size()); i++) {
-        res += v1[i] * v2[i];
-    }
-    return res;
-}
-
 optional<bool> threshold_ad(const vector<double> &measurements, double abs_low, double abs_high) {
     if (measurements.empty()) {
         return {};
@@ -41,20 +32,6 @@ optional<bool> persist_ad(const vector<double> &measurements, int window_size, d
     return threshold_ad({abs(m2 - m1)}, numeric_limits<double>::lowest(), max_diff);
 }
 
-optional<bool>
-auto_regression_ad(const vector<double> &measurements, const vector<double> &coefficients, double max_diff) {
-    int window_size = int(coefficients.size());
-    if (static_cast<int>(measurements.size()) < window_size + 1) {
-        return {};
-    }
-    vector<double> window(measurements.end() - window_size - 1, measurements.end() - 1);
-    reverse(window.begin(), window.end());
-
-    double last_v = measurements.back();
-    double prediction = dot_product(window, coefficients);
-    return threshold_ad({abs(prediction - last_v)}, numeric_limits<double>::lowest(), max_diff);
-}
-
 AnomalyData FullAnomalyDetector::add(uint64_t timestamp, double measurement) {
     uint64_t current_sampled_window_time = timestamp - timestamp % SAMPLING_SECONDS;
     if (!last_window.empty() && sampled_window_time != current_sampled_window_time) {
@@ -72,18 +49,14 @@ AnomalyData FullAnomalyDetector::add(uint64_t timestamp, double measurement) {
 
     auto persist_long_detection = persist_ad(analyzed_window, PERSIST_LONG_WINDOW, PERSIST_LONG_MAX_DIFF);
     auto persist_short_detection = persist_ad(analyzed_window, PERSIST_SHORT_WINDOW, PERSIST_SHORT_MAX_DIFF);
-    auto autoregression_detection = auto_regression_ad(analyzed_window, AUTO_COEFFICIENTS, AUTO_MAX_DIFF);
-    auto threshold_detection = threshold_ad(analyzed_window, -100, 100);
 
     bool is_anomaly = false;
     int idx = 0;
     vector<std::string> names = {
             "persist_long",
             "persist_short",
-            "auto",
-            "threshold"
     };
-    for (auto res : {persist_long_detection, persist_short_detection, autoregression_detection, threshold_detection}) {
+    for (auto res : {persist_long_detection, persist_short_detection}) {
         if (res.has_value() && res.value()) {
             is_anomaly = true;
             std::cerr << "Anomaly for " << timestamp << ": " << names[idx] << "\n";
@@ -94,15 +67,14 @@ AnomalyData FullAnomalyDetector::add(uint64_t timestamp, double measurement) {
     double diff = measurement - previous_measurement;
     if (is_anomaly) {
         obstruction += diff;
-    } else if (abs(obstruction) > EPS){
-        double previous_obstruction = obstruction;
-        if (signbit(diff) != signbit(obstruction)) {
-            obstruction += diff;
+    } else {
+        if(measurement - obstruction < 0){
+            obstruction = measurement;
         }
-        if (signbit(previous_obstruction) != signbit(obstruction)) {
-            obstruction = 0;
+        if(obstruction < 0){
+            obstruction = std::min(0.0, measurement);
         }
     }
     previous_measurement = measurement;
-    return AnomalyData{abs(obstruction) > 0.5, measurement - obstruction};
+    return AnomalyData{abs(obstruction) > 2, measurement - obstruction};
 }
